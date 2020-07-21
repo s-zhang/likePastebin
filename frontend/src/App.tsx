956b0a777ab3e6db748a2b43bf2e818ca5b3d007
Humayara918 <64216341+Humayara918@users.noreply.github.com>
2020-07-21 15:28:47 -0400

    updating

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,9 @@ function App() {
       
       
       <textarea ng-model="myTextArea" id="myTextArea">Put your message here:)</textarea>
-      <input type="file" id="myFile" name="filename"></input>
-      <input type="submit"></input>
+
+  
+      
       <h1>Privacy</h1>
       <input type="radio" id="Radio2" name = "private"/>
       <label>private</label><br></br>
@@ -26,12 +27,35 @@ function App() {
       <input type="radio" id="Radio4" name = "Never"/>
       <label>Never</label><br></br>
       <input type="radio" id="Radio5" name = "Time"/> 
-      <label>Time</label><input type="text" id="dropdown1" /> 
-      <br></br>
+
+       <label>Time</label>
+       <select id="Radio5">
+         <option value="Select">Select Time</option>
+         <option value="hour">1 hour</option>
+         <option value="hour">2 hour</option>
+         <option value="hour">3 hour</option>
+       </select>
+       
+       
+     
+      
+
+      
+        {/*} <input type="file"    name="filename"></input>
+      <input type="submit"></input> */}
+<br></br>
+
+<label htmlFor="password" id="pass">password</label>
+<input type="text" id="password"></input>
+
+       
+       {/*<h3>Password</h3><input type="text" id="Password"></input> */}
+
+       
      { /*<label>Password (8 characters minimum):</label><input type="password" id="pass" name="password"></input>*/}
       <input type="submit" id="create"></input>
       
-      <h3>Encripted Link</h3>
+     {/* <h3>Encripted Link</h3> */}
     
       {/*
        
