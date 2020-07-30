b31bf4f30c99ac99d1ff8852454ca004d787ed40
Humayara918 <64216341+Humayara918@users.noreply.github.com>
2020-07-30 19:47:20 -0400

    iD

--- frontend/src/message.tsx
+++ frontend/src/message.tsx
@@ -7,17 +7,22 @@ var SHA256 = require("crypto-js/sha256");
 var CryptoJS = require("crypto-js");
 
 
-class message extends React.Component<any, any>  {
+class Message extends React.Component<any, any>  {
 
   constructor(props: any) {
     super(props);
-    this.state = {
+    this.state = { 
+      data: {
+        title:"",
+        message:"",
+        expiration:""
+      }
     };
 
     this.handleClick= this.handleClick.bind(this)
   }
 
-  componentDidMount() {
+  componentWillMount() {
     const requestHeaders: HeadersInit = new Headers();
     requestHeaders.set('Content-Type', 'application/json',);
 
@@ -26,8 +31,8 @@ class message extends React.Component<any, any>  {
           .then(response => response.json())
           .then(data => this.setState(
             { 
-              postId: data.id,
-              text: JSON.stringify(data)
+            
+              data: data
             }));
   }
 
@@ -64,12 +69,12 @@ class message extends React.Component<any, any>  {
           <div className="info">
             <div className="infoTop">
               <p className="infoDescribe" id="privacyInfo">Private</p>
-              <p className="infoDescribe right" id="expireCountdown">(expiration countdown)</p>
+              <p className="infoDescribe right" id="expireCountdown">{this.state.data.expiration}</p>
               <p className="infoDescribe right">Expires: </p>
             </div>
             <div className="infoContent">
-              <h1 id="titleInfo">Title</h1>
-              <h5 id="textInfo">{ this.state.text }</h5>
+              <h1 id="titleInfo">{this.state.data.title}</h1>
+              <h5 id="textInfo">{ this.state.data.message }</h5>
             </div>
           </div>
 
@@ -79,4 +84,4 @@ class message extends React.Component<any, any>  {
   }
 };
 
-export default message;
+export default Message;
